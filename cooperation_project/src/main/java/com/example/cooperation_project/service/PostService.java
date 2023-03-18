99498fb43d9b4aed4d120a8dff26827696f62518
parker4942 <parkmj4312@gmail.com>
2023-03-18 10:35:55 +0900

    포스트 코멘트 좋아요 시크릿키

--- cooperation_project/src/main/java/com/example/cooperation_project/service/PostService.java
+++ cooperation_project/src/main/java/com/example/cooperation_project/service/PostService.java
@@ -2,23 +2,31 @@ package com.example.cooperation_project.service;
 
 import com.example.cooperation_project.dto.PostRequestDto;
 import com.example.cooperation_project.dto.PostResponseDto;
+import com.example.cooperation_project.entity.LovePost;
 import com.example.cooperation_project.entity.Post;
 import com.example.cooperation_project.entity.User;
 import com.example.cooperation_project.exception.ApiException;
 import com.example.cooperation_project.exception.ExceptionEnum;
+import com.example.cooperation_project.repository.LovePostRepository;
 import com.example.cooperation_project.repository.PostRepository;
+import com.example.cooperation_project.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
 
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final LovePostRepository lovePostRepository;
 
     @Transactional
     public PostResponseDto createPost(PostRequestDto requestDto, User user){
@@ -45,5 +53,50 @@ public class PostService {
         return new PostResponseDto(post);
     }
 
+    @Transactional
+    public ResponseEntity<Map<String, HttpStatus>> loveOk(Long id, User user) {
 
+        User user1 = userRepository.findById(user.getUserId()).orElseThrow(
+                () -> new IllegalArgumentException("유저가 존재하지 않습니다.")
+        );
+        Post post = postRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("게시글이 존재하지 않습니다.")
+        );
+
+        List<LovePost> boardLoveList = user1.getLovePostList();
+
+        if (user != null) {
+
+            for (LovePost lovePost : boardLoveList) {
+                if (lovePost.getPost().getPost_Id() == post.getPost_Id() && lovePost.getUser().getUserId() == user1.getUserId()) {
+                    if (lovePost.isLove() == false) {
+                        lovePost.update();
+                        post.LoveOk();
+                        return new ResponseEntity("게시글을 좋아요 했습니다.", HttpStatus.OK);
+                    } else {
+                        lovePost.update();
+                        post.LoveCancel();
+                        return new ResponseEntity("좋아요를 취소 했습니다.", HttpStatus.OK);
+                    }
+                } else {
+                    LovePost lovePost1 = new LovePost(post, user1);
+                    lovePostRepository.save(lovePost1);
+                    lovePost1.update();
+                    post.LoveOk();
+                    return new ResponseEntity("게시글을 좋아요 했습니다.", HttpStatus.OK);
+                }
+            }
+            if(boardLoveList.size() == 0){
+                LovePost lovePost1 = new LovePost(post, user1);
+                lovePostRepository.save(lovePost1);
+                lovePost1.update();
+                post.LoveOk();
+                return new ResponseEntity("게시글을 좋아요 했습니다.", HttpStatus.OK);
+            }
+
+        } else {
+            throw new IllegalArgumentException("로그인 유저만 좋아요할 수 있습니다.");
+        }
+        return null;
+    }
 }
