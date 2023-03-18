403de041f16b6dbdccd76772e12ffee9f3135113
yujin-17 <lyjyellow@gmail.com>
2023-03-18 10:06:24 +0900

    postService Completion

--- cooperation_project/src/main/java/com/example/cooperation_project/service/PostService.java
+++ cooperation_project/src/main/java/com/example/cooperation_project/service/PostService.java
@@ -1,13 +1,16 @@
 package com.example.cooperation_project.service;
 
+import com.example.cooperation_project.dto.MsgCodeResponseDto;
 import com.example.cooperation_project.dto.PostRequestDto;
 import com.example.cooperation_project.dto.PostResponseDto;
 import com.example.cooperation_project.entity.Post;
 import com.example.cooperation_project.entity.User;
+import com.example.cooperation_project.entity.UserRoleEnum;
 import com.example.cooperation_project.exception.ApiException;
 import com.example.cooperation_project.exception.ExceptionEnum;
 import com.example.cooperation_project.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -44,4 +47,32 @@ public class PostService {
         );
         return new PostResponseDto(post);
     }
-}
+
+    @Transactional
+    public PostResponseDto update(Long post_Id, PostRequestDto postRequestDto, User user){
+        Post post = postRepository.findById(post_Id).orElseThrow(
+                () -> new ApiException(ExceptionEnum.NOT_FOUND_POST_ALL)
+        );
+
+        if(post.getUser().getUserId().equals(user.getUserId()) || user.getRole() == UserRoleEnum.ADMIN){
+            post.update(postRequestDto);
+            return new PostResponseDto(post);
+        }
+        return null;
+    }
+
+    @Transactional
+    public MsgCodeResponseDto delete(Long post_Id, User user) {
+        MsgCodeResponseDto responseDto = new MsgCodeResponseDto();
+        Post post = postRepository.findById(post_Id).orElseThrow(
+                () -> new ApiException(ExceptionEnum.NOT_FOUND_POST_ALL)
+        );
+        if (post.getUser().getUserId().equals(user.getUserId()) || user.getRole() == UserRoleEnum.ADMIN) {
+            postRepository.deleteById(post_Id);
+            responseDto.setResult("게시글 삭제 성공", HttpStatus.OK.value());
+            return responseDto;
+        }
+        responseDto.setResult("게시글 삭제 실패", HttpStatus.BAD_REQUEST.value());
+        return responseDto;
+    }
+    }
