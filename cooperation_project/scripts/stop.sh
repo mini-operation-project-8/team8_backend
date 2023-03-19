120a734d5ad143a30138f70882e2e2d60e06c6bb
fghij7410 <fghij7410@gmail.com>
2023-03-19 12:24:58 +0900

    CI/CD 테스트

--- cooperation_project/scripts/stop.sh
+++ cooperation_project/scripts/stop.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu"
+PROJECT_ROOT="/home/ubuntu/app"
 JAR_FILE="$PROJECT_ROOT/cooperation_project-0.0.1-SNAPSHOT.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
