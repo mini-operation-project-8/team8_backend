ea0eaea97f5c763bc86212db8246c89ca1c707a3
fghij7410 <fghij7410@gmail.com>
2023-03-19 09:20:59 +0900

    주석변경 및 gradlew설정변경

--- cooperation_project/src/main/java/com/example/cooperation_project/config/WebSecurityConfig.java
+++ cooperation_project/src/main/java/com/example/cooperation_project/config/WebSecurityConfig.java
@@ -44,10 +44,12 @@ public class WebSecurityConfig {
 
         http.authorizeRequests()
                 .antMatchers(("/chitchat/**")).permitAll()
-//                .antMatchers("/api/user/login").permitAll() // permitAll() : 모든사람이 사용가능하게 만들어준다. 관리자만, 회원만 접근 가능하게도 만들수 있다. 제일위에는 무엇?
-//                .antMatchers(HttpMethod.GET,"/api/posts").permitAll() // 모든 유저는 게시글 조회가 가능하게 해야한다.
-//                .antMatchers(HttpMethod.GET,"/api/post/{id}").permitAll() // 세부적으로 나눠서 모든 유저를 접근 가능하게 만든다. 상세조회 {id}가 맞는지
-//                .antMatchers(HttpMethod.POST,"/api/user/signup").permitAll() // 회원가입
+/*
+                .antMatchers("/api/user/login").permitAll() // permitAll() : 모든사람이 사용가능하게 만들어준다. 관리자만, 회원만 접근 가능하게도 만들수 있다. 제일위에는 무엇?
+                .antMatchers(HttpMethod.GET,"/api/posts").permitAll() // 모든 유저는 게시글 조회가 가능하게 해야한다.
+                .antMatchers(HttpMethod.GET,"/api/post/{id}").permitAll() // 세부적으로 나눠서 모든 유저를 접근 가능하게 만든다. 상세조회 {id}가 맞는지
+                .antMatchers(HttpMethod.POST,"/api/user/signup").permitAll() // 회원가입
+*/
                 .anyRequest().authenticated()
                 .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
