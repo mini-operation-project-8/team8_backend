d40baa9b5ee5272bc3b6c613f70549be30cf9308
parker4942 <parkmj4312@gmail.com>
2023-03-20 20:57:00 +0900

    스프링 웹시큐리티config 동기화

--- cooperation_project/src/main/java/com/example/cooperation_project/config/WebSecurityConfig.java
+++ cooperation_project/src/main/java/com/example/cooperation_project/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -30,37 +31,46 @@ public class WebSecurityConfig {
 
     private final JwtUtil jwtUtil;
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer(){
+    @Profile("db-local")
+    public WebSecurityCustomizer localWebSecurityCustomizer() {
+        // 로컬 환경 개발 : h2-console 사용 및 resources 접근 허용 설정
         return (web) -> web.ignoring()
-//                .requestMatchers(PathRequest.toH2Console())
+                .requestMatchers(PathRequest.toH2Console())
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    @Profile("db-dev")
+    public WebSecurityCustomizer devWebSecurityCustomizer() {
+        // mysql 배포 환경 : resources 접근 허용 설정
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
 
+        // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.authorizeRequests()
-                .antMatchers(("/chitchat/**")).permitAll()
-/*
-                .antMatchers("/api/user/login").permitAll() // permitAll() : 모든사람이 사용가능하게 만들어준다. 관리자만, 회원만 접근 가능하게도 만들수 있다. 제일위에는 무엇?
-                .antMatchers(HttpMethod.GET,"/api/posts").permitAll() // 모든 유저는 게시글 조회가 가능하게 해야한다.
-                .antMatchers(HttpMethod.GET,"/api/post/{id}").permitAll() // 세부적으로 나눠서 모든 유저를 접근 가능하게 만든다. 상세조회 {id}가 맞는지
-                .antMatchers(HttpMethod.POST,"/api/user/signup").permitAll() // 회원가입
-*/
+                .antMatchers("/chitchat/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/games/").permitAll()
                 .anyRequest().authenticated()
                 .and().cors()
+                // JWT 인증/인가를 사용하기 위한 설정
                 .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
-        //http.formLogin().loginPage("@#$@#$").permitAll();
-        http.formLogin().permitAll();
+
+        // http.formLogin().loginPage("/api/user/login").permitAll();
+
         http.exceptionHandling().accessDeniedPage("/api/user/forbidden");
 
         return http.build();
