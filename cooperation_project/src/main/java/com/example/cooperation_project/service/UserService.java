ecd6b4aadcd595a774ceaf309b99330101d7e5f1
joinjun <dignzh@naver.com>
2023-03-21 18:01:19 +0900

    UserService 리팩토링

--- cooperation_project/src/main/java/com/example/cooperation_project/service/UserService.java
+++ cooperation_project/src/main/java/com/example/cooperation_project/service/UserService.java
@@ -2,11 +2,11 @@ package com.example.cooperation_project.service;
 
 import com.example.cooperation_project.dto.MsgCodeResponseDto;
 import com.example.cooperation_project.dto.auth.SignupRequestDto;
-import com.example.cooperation_project.entity.Post;
 import com.example.cooperation_project.entity.User;
 import com.example.cooperation_project.entity.UserRoleEnum;
 import com.example.cooperation_project.exception.DuplicateUserException;
 import com.example.cooperation_project.exception.NotAuthException;
+import com.example.cooperation_project.exception.NotFoundUserException;
 import com.example.cooperation_project.jwt.JwtUtil;
 import com.example.cooperation_project.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -23,15 +23,15 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final  JwtUtil jwtUtil;
-    private final PasswordEncoder passwordEncoder;
+    private final PasswordEncoder encoder;
 
     private static final String ADMIN_TOKEN = "admin";
 
     @Transactional
-    public MsgCodeResponseDto signup(SignupRequestDto dto){
+    public void signup(SignupRequestDto dto){
 
         String userId = dto.getUserId();
-        String password = passwordEncoder.encode(dto.getPassword());
+        String password = encoder.encode(dto.getPassword());
 
         Optional<User> found = userRepository.findByUserId(userId);
 
@@ -48,26 +48,24 @@ public class UserService {
         User user = new User(userId, password, role);
 
         userRepository.save(user);
-
-        return new MsgCodeResponseDto("회원가입 성공");
     }
 
     @Transactional
-    public MsgCodeResponseDto login(SignupRequestDto signupRequestDto , HttpServletResponse httpServletResponse){
-        String userId = signupRequestDto.getUserId();
+    public String login(SignupRequestDto dto){
+        String userId = dto.getUserId();
 
         User user = userRepository.findByUserId(userId).orElseThrow(
-                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+                () -> new NotFoundUserException("등록된 사용자가 없습니다.")
         );
-        String encodePassword = user.getPassword();
-        boolean isPasswordMatch = passwordEncoder.matches(signupRequestDto.getPassword(), encodePassword);
-        if(!isPasswordMatch){
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다");
-        }
-        httpServletResponse.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUserId(), user.getRole()));
 
-        MsgCodeResponseDto result = new MsgCodeResponseDto("로그인 성공");
-        return result;
+        String encodePassword = user.getPassword();
+
+        if(!encoder.matches(dto.getPassword(), encodePassword)){
+
+            throw new NotAuthException("인증 정보가 맞지 않아 실패.");
+        }
+
+        return jwtUtil.createToken(user.getUserId(), user.getRole());
     }
 
     private boolean isAdmin(SignupRequestDto dto){
