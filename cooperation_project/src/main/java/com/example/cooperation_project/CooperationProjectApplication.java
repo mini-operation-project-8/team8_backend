25b69e53e6d9a0035a57804e8f9ec365bf4b6d6f
joinjun <dignzh@naver.com>
2023-03-22 23:19:17 +0900

    post total 갯수 반환 구현

--- cooperation_project/src/main/java/com/example/cooperation_project/CooperationProjectApplication.java
+++ cooperation_project/src/main/java/com/example/cooperation_project/CooperationProjectApplication.java
@@ -1,5 +1,7 @@
 package com.example.cooperation_project;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +11,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class CooperationProjectApplication {
+
+    @PostConstruct
+    public void started(){
+
+        TimeZone.setDefault(TimeZone.getTimeZone("KST"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(CooperationProjectApplication.class, args);
     }
